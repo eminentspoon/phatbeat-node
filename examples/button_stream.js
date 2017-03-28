9da31d9bddaf7af71b611eb3e4cbbd0f245e9526
eminentspoon <contact@eminentspoon.com>
2017-03-28 21:00:35 +0000

    updates to button examples

--- examples/button_stream.js
+++ examples/button_stream.js
@@ -3,15 +3,5 @@ let buttons = phat.getButtonPins();
 
 for(let i = 0; i < buttons.length; i++){
     let stream = phat.buttonStream(buttons[i].pin);
-    stream.on("pinChange", function(pin, pinState){
-        console.log("Pin " + pin + " is now " + (pinState === 1 ? "pressed" : "released"));
-    });
-
-    stream.on("monitoring", function(pin){
-        console.log("Pin " + pin + " is now being monitored for status changes. Press and hold button for at least 5 seconds to stop monitoring.");
-    });
-
-    stream.on("end", function(pin){
-        console.log("Pin " + pin + " stream closed.");
-    });
+    stream.pipe(process.stdout);
 }
